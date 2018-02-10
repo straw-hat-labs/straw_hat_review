211bd47dbbec9c74f7b7294b612878e8755b1215
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2018-02-09 19:58:44 -0800

    add query modules

---
+++ lib/straw_hat_review/query/review_accomplishment.ex
@@ -0,0 +1,14 @@
+defmodule StrawHat.Review.Query.ReviewAccomplishmentQuery do
+  @moduledoc false
+
+  import Ecto.Query, only: [from: 2]
+
+  @spec get_by(ReviewAccomplishment.t(), Integer.t(), [Integer.t()]) :: Ecto.Query.t()
+  def get_by(query, review_id, accomplishment_ids) do
+    from(
+      review_accomplishmen in query,
+      where: review_accomplishmen.review_id == ^review_id,
+      where: review_accomplishmen.accomplishmen_id in ^accomplishment_ids
+    )
+  end
+end
