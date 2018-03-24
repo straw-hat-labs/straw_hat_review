b61b0d252beb73f97d9cd4f5d0c22b3525cb05ff
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2018-03-23 17:55:04 -0700

    update achievement migration

--- priv/repo/migrations/20180218015930_create_achievements_table.exs
+++ priv/repo/migrations/20180218015930_create_achievements_table.exs
@@ -2,7 +2,7 @@ defmodule StrawHat.Review.Repo.Migrations.CreateAchievementsTable do
   use Ecto.Migration
 
   def change do
-    create table(:achievements, primary_key: false) do
+    create table(:achievements) do
       add(:owner_id, :string, null: false)
       add(:achievement_badge_id, references(:achievement_badges), primary_key: true, on_delete: :delete_all)
     end
