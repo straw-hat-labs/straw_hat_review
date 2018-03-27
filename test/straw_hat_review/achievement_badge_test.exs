20c872a508bc512deab95d4bdd7d79da6eb50719
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2018-03-26 18:15:21 -0700

    update the test

--- test/straw_hat_review/achievement_badge_test.exs
+++ test/straw_hat_review/achievement_badge_test.exs
@@ -1,38 +1,44 @@
-defmodule StrawHat.Review.Test.AchievementBadgeTest do
+defmodule StrawHat.Review.Test.AchievementBadgesTest do
   use StrawHat.Review.Test.DataCase, async: true
-  alias StrawHat.Review.AchievementBadge
+  alias StrawHat.Review.AchievementBadges
 
-  test "get achievement badge by id" do
-    achievement_badge = insert(:achievement_badge)
-    assert AchievementBadge.get_achievement_badge(achievement_badge.id) != nil
+  describe "find_achievement_badge/1" do
+    test "with valid id return the relative achievement badge" do
+      achievement_badge = insert(:achievement_badge)
+
+      assert {:ok, _achievement_badge} = AchievementBadges.find_achievement_badge(achievement_badge.id)
+    end
+
+    test "with invalid id shouldn't find the achievement badge" do
+      assert {:error, _reason} = AchievementBadges.find_achievement_badge(8347)
+    end
   end
 
-  test "get achievement badge with invalid id" do
-    assert {:error, _reason} = AchievementBadge.find_achievement_badge(836_747)
-  end
-
-  test "list per page" do
+  test "get_achievement_badges/1 list the achievement badges per page" do
     insert_list(10, :achievement_badge)
-    achievement_badge = AchievementBadge.get_achievement_badges(%{page: 2, page_size: 5})
+    achievement_badge = AchievementBadges.get_achievement_badges(%{page: 2, page_size: 5})
+
     assert achievement_badge.total_entries == 10
   end
 
-  test "create achievement badge" do
+  test "create_achievement_badge/1 with valid inputs creates a achievement badge" do
     params = params_for(:achievement_badge)
-    assert {:ok, _achievement_badge} = AchievementBadge.create_achievement_badge(params)
+
+    assert {:ok, _achievement_badge} = AchievementBadges.create_achievement_badge(params)
   end
 
-  test "update achievement badge" do
+  test "update_achievement_badge/2 with valid inputs updates a achievement badge" do
     achievement_badge = insert(:achievement_badge)
 
     {:ok, achievement_badge} =
-      AchievementBadge.update_achievement_badge(achievement_badge, %{name: "Five client in three hours"})
+      AchievementBadges.update_achievement_badge(achievement_badge, %{name: "Five client in three hours"})
 
     assert achievement_badge.name == "Five client in three hours"
   end
 
-  test "delete achievement badge" do
+  test "destroy_achievement_badge/1 with a found review destroys the achievement badge" do
     achievement_badge = insert(:achievement_badge)
-    assert {:ok, _} = AchievementBadge.destroy_achievement_badge(achievement_badge)
+
+    assert {:ok, _} = AchievementBadges.destroy_achievement_badge(achievement_badge)
   end
 end
--- test/straw_hat_review/review_tags_test.exs
+++ test/straw_hat_review/review_tags_test.exs
@@ -1,6 +1,6 @@
 defmodule StrawHat.Review.Test.ReviewTagsTest do
   use StrawHat.Review.Test.DataCase, async: true
-  alias StrawHat.Review.Schema.ReviewTag
+  alias StrawHat.Review.ReviewTag
 
   test "validate changeset with nil data" do
    assert %Ecto.Changeset{errors: _errors}  = %ReviewTag{} |> ReviewTag.changeset(%{})
