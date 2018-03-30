ef2d0cebd9332767398d7348bad02a4950d5f7e9
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-03-29 22:07:35 -0700

    cleaning up aspects

--- lib/straw_hat_review/aspect/aspects.ex
+++ lib/straw_hat_review/aspect/aspects.ex
@@ -7,14 +7,14 @@ defmodule StrawHat.Review.Aspects do
   alias StrawHat.Review.Aspect
 
   @doc """
-  Get the list of aspects.
+  Gets the list of aspects.
   """
   @since "1.0.0"
   @spec get_aspects(Scrivener.Config.t()) :: Scrivener.Page.t()
   def get_aspects(pagination \\ []), do: Repo.paginate(Aspect, pagination)
 
   @doc """
-  Create aspect.
+  Creates aspect.
   """
   @since "1.0.0"
   @spec create_aspect(Aspect.aspect_attrs()) :: {:ok, Aspect.t()} | {:error, Ecto.Changeset.t()}
@@ -25,7 +25,7 @@ defmodule StrawHat.Review.Aspects do
   end
 
   @doc """
-  Update aspect.
+  Updates aspect.
   """
   @since "1.0.0"
   @spec update_aspect(Aspect.t(), Aspect.aspect_attrs()) ::
@@ -37,35 +37,30 @@ defmodule StrawHat.Review.Aspects do
   end
 
   @doc """
-  Destroy aspect.
+  Destroys aspect.
   """
   @since "1.0.0"
   @spec destroy_aspect(Aspect.t()) :: {:ok, Aspect.t()} | {:error, Ecto.Changeset.t()}
   def destroy_aspect(%Aspect{} = aspect), do: Repo.delete(aspect)
 
   @doc """
-  Find aspect by `id`.
+  Finds aspect by `id`.
   """
   @since "1.0.0"
   @spec find_aspect(Integer.t()) :: {:ok, Aspect.t()} | {:error, Error.t()}
   def find_aspect(aspect_id) do
-    case get_aspect(aspect_id) do
-      nil ->
-        error =
-          Error.new(
-            "straw_hat_review.aspect.not_found",
-            metadata: [aspect_id: aspect_id]
-          )
-
-        {:error, error}
-
-      aspect ->
-        {:ok, aspect}
-    end
+    aspect_id
+    |> get_aspect()
+    |> StrawHat.Response.from_value(
+      Error.new(
+        "straw_hat_review.aspect.not_found",
+        metadata: [aspect_id: aspect_id]
+      )
+    )
   end
 
   @doc """
-  Get aspect by `id`.
+  Gets aspect by `id`.
   """
   @since "1.0.0"
   @spec get_aspect(Integer.t()) :: Aspect.t() | nil | no_return
