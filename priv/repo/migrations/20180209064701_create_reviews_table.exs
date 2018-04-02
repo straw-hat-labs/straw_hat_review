b4ce62a28dff3646fa11e0344c0f9399e53d4971
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2018-04-01 21:37:58 -0700

    update review migration

--- priv/repo/migrations/20180209064701_create_reviews_table.exs
+++ priv/repo/migrations/20180209064701_create_reviews_table.exs
@@ -3,20 +3,13 @@ defmodule StrawHat.Review.Repo.Migrations.CreateReviewsTable do
 
   def change do
     create table(:reviews) do
-      add(:date, :utc_datetime, null: false)
-      add(:score, :integer, null: false)
-
       # the object or user that receive the review
       add(:reviewee_id, :string, null: false)
-
       # the user that make the comment
       add(:reviewer_id, :string, null: false)
-
-      # tag for mark the review for example customer, performer etc...
-      add(:type, :string)
-
       add(:comment, :string, null: false)
-      add(:review_id, references(:reviews), null: false, on_delete: :delete_all)
+
+      timestamps()
     end
   end
 end
