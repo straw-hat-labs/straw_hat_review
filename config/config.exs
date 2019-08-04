775f7972fd5b00e86891112677645a84ffda42a2
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-03 17:05:13 -0700

    Add workflows

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :straw_hat_review, ecto_repos: [StrawHat.Review.Repo]
 config :straw_hat_review, StrawHat.Review.Repo,
   database: "straw_hat_review_test",
   username: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :arc, storage: Arc.Storage.Local
