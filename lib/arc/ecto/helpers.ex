844fabc356ebaaa234f1429452a5e2d718b53d8a
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-19 06:24:54 -0700

    Fix formatter

--- lib/arc/ecto/helpers.ex
+++ lib/arc/ecto/helpers.ex
@@ -8,12 +8,12 @@ defmodule Arc.Ecto.Changeset.Helpers do
         nil
 
       {key, _value}, _ when is_binary(key) ->
-        raise ArgumentError, "expected params to be a map with atoms or string keys, " <>
-                             "got a map with mixed keys: #{inspect params}"
+        raise ArgumentError,
+              "expected params to be a map with atoms or string keys, " <>
+                "got a map with mixed keys: #{inspect(params)}"
 
       {key, value}, acc when is_atom(key) ->
         Map.put(acc || %{}, Atom.to_string(key), value)
-
     end) || params
   end
 end
