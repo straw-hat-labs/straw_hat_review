e81d5959aaf24b5a50beeaa03dc2ec6f87f57678
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-19 06:20:46 -0700

    Fix db connection

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,13 @@
 use Mix.Config
 
+db_hostname = if System.get_env("CI"), do: "db", else: "localhost"
+
 config :straw_hat_review, ecto_repos: [StrawHat.Review.Repo]
 
 config :straw_hat_review, StrawHat.Review.Repo,
   database: "straw_hat_review_test",
   username: "postgres",
-  hostname: "db",
+  hostname: db_hostname,
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :arc, storage: Arc.Storage.Local
