9b05e2febcf868f1aa714fb0413af563a8769167
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-29 20:20:41 -0700

    Fix db setup

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :straw_hat_review, ecto_repos: [StrawHat.Review.Repo]
 config :straw_hat_review, StrawHat.Review.Repo,
   database: "straw_hat_review_test",
   username: "postgres",
-  hostname: System.get_env("CI_DB_HOSTNAME", "localhost"),
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :arc, storage: Arc.Storage.Local
